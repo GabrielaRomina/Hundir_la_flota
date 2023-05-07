25afa0f1983c810a9c45cbd50aeba403a0b67953
GabrielaRomina <132007081+GabrielaRomina@users.noreply.github.com>
2023-05-07 18:04:11 +0200

    Add files via upload

--- Hundir_la_flota/clases.py
+++ Hundir_la_flota/clases.py
@@ -3,14 +3,19 @@ import random
 import funciones as fc
 
 class Tablero:
+    """ Clase tablero
+    Dimensiones de 10x10
+    y se generan siempre dos tableros, uno vacío y otro con barcos colocados."""
     dimensiones = (10,10)
     tablero = np.full((10,10), " ")
     tablero_oculto = fc.generar_barcos_aleatorios()
 
     def __init__(self, id_jugador):
+        # Id de jugador
         self.id_jugador = id_jugador
     
     def disparar(self, fila, columna):
+        # Método disparo, va modificando las filas y las columnas del tablero las coordenadas del disparo
         if self.tablero[fila][columna] == "O":
             self.tablero[fila][columna] = "X"
             self.tablero_oculto[fila][columna] = "X"
@@ -20,5 +25,6 @@ class Tablero:
             self.tablero_oculto[fila][columna] = "-"
             print("Agua")
 
+#Objetos de la clase tablero, del jugador y de la máquina
 jugador = Tablero("jugador")
 maquina = Tablero("maquina")
